2a09596699bdd7c848a3198a06f07ae5e63fa32f
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-01-05 14:55:31 -0300

    02:08 - arrow function

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-function soma(valor1, valor2) {
-    console.log('executou:', valor1, valor2);
-    let resultado = valor1 + valor2;
-    return resultado;
-}
+// function soma(valor1, valor2) {
+//     console.log('executou:', valor1, valor2);
+//     let resultado = valor1 + valor2;
+//     return resultado;
+// }
 
-let myFunction = function () {
-    console.log('executou');
-}
+let soma = (x, y) => x + y;
 
-myFunction();
+console.log(soma(150, 50));
 
 
 
