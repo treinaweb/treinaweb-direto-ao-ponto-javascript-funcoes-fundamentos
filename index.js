8d61d6f2ed52d22780f6880f06e44d4f624a6f3d
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-01-05 14:54:45 -0300

    02:07 - avançando com funções anônimas

--- index.js
+++ index.js
@@ -4,14 +4,11 @@ function soma(valor1, valor2) {
     return resultado;
 }
 
-let myFunction1 = soma;
+let myFunction = function () {
+    console.log('executou');
+}
 
-let myFunction2 = soma;
-
-let resultado1 = myFunction1(150, 50);
-let resultado2 = myFunction2(30, 15);
-
-console.log('resultado', resultado1, resultado2);
+myFunction();
 
 
 
