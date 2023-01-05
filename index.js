40e8b5f4dd901d0efd5bfa3344444f7966975e8f
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-01-05 14:50:02 -0300

    02:04 - parâmetros e argumentos da função

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-function soma() {
-    console.log(10 + 5);
+function soma(valor1, valor2) { // parâmetro
+    console.log('a soma é:', valor1 + valor2);
 }
 
-function myFunction() {
-    console.log('olá mundo');
-    soma();
+function subtracao(x, y) {
+    console.log('a subtração é:', x - y);
+}
+
+function myFunction(value = '', numero = 0) {
+    console.log(value, numero);
 }
 
 myFunction();
+
+myFunction('novo valor');
+
+myFunction('novo valor', 50);
+
 
 
 
